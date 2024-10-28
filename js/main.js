0efba5ab071acb501c31547aa363928e6854e6bd
Gargenziano03 <gargenziano03@gmail.com>
2024-10-28 16:11:47 +0100

    add argomento

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ if (!pin) {
 }
 
 console.log(process.env.PIN_KEY);
+
+const testo = process.argv[2];
+if (!testo) {
+    console.log('inserisci un argomento');
+    process.exit(1)   
+}
+console.log(testo.toUpperCase());
+console.log(process.argv);
+
