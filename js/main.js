06a7a8b286226c1b6426d2ac70c2dff6c177bf32
Gargenziano03 <gargenziano03@gmail.com>
2024-10-28 15:38:40 +0100

    add pin + process + env

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log('hello node js');
+
+const pin = process.env.PIN_KEY;
+if (!pin) {
+    console.log('No PIN key found, exitin');
+    process.exit(1);
+}
+
+console.log(process.env.PIN_KEY);
